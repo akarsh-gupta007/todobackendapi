2dadea2b5c071b9ebc38b014e2e5735abfd111db
Akarshgu <akarshgupta705415@gmail.com>
2023-03-13 17:27:51 +0530

    ok

---
+++ database/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const Connection = () => {
+    const MONGODB_URI = `mongodb+srv://akarshgupta007:Ak161998@cluster0.aewqbvm.mongodb.net/?retryWrites=true&w=majority  `;
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
