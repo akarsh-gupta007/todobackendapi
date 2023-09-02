0eac38a111d4fa2167c98514deee4d8cc9b0451a
Akarsh gupta <98002276+akarsh-gupta007@users.noreply.github.com>
2023-09-03 02:23:00 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { addTodo, getAllTodos, toggleTodoDone, updateTodo, deleteTodo } from '../controller/todo-controller.js';
 const route = express.Router();
 
-route.post('/todos', addTodo)
+route.post('/createtodos', addTodo)
 route.get('/todos', getAllTodos);
 route.get('/todos/:id', toggleTodoDone);
 route.put('/todos/:id', updateTodo);
